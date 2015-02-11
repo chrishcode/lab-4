d0b7d5ae3c772033b580eb801ac4d07c6c32ee49
unknown <chrwoh14@IDED308-10754.hh.se>
2015-02-11 11:57:49 +0100

    har fixat lite coola fadeiIns

--- javascript.js
+++ javascript.js
@@ -23,11 +23,28 @@ $(document).ready(function(){
 	$("#bilder").mouseenter(function(){
 		$("#dropdown-panel").slideDown(300);
 		$("#dropdown-panel").empty();
-		$("#dropdown-panel").append("<h1>BILDER</h1><img src='http://animalia-life.com/data_images/cat/cat2.jpg'>");
+		$("#dropdown-panel").append("<h1>BILDER</h1>");
 		$("#dropdown-panel").css("background-color", "#C14FD8");
 	});
 	
 	$("#dropdown-panel").mouseleave(function(){
 		$("#dropdown-panel").slideUp(300);
+	});
+	
+	$("#start").fadeIn(3000);
+	
+	$(window).scroll(function() {
+		if($(this).scrollTop() > 400) {
+			$("#p1").fadeIn(1000);
+		}
+		
+		if($(this).scrollTop() > 700) {
+			$("#p2").fadeIn(1000);
+		}
+		
+		if($(this).scrollTop() > 1150) {
+			$("#p3").fadeIn(1000);
+		}
+		
 	});
 });
