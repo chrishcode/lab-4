e3b6af5407cb84a6b6161ac7f9649eb1fd52dc70
unknown <chrwoh14@IDED308-10754.hh.se>
2015-02-11 10:59:40 +0100

    fixat till dropdown menyn

--- javascript.js
+++ javascript.js
@@ -1,5 +1,33 @@
 $(document).ready(function(){
-	$("#menubar a").click(function(){
-		$("#dropdown-panel").slideToggle(300);
+	$("#hem").mouseenter(function(){
+		$("#dropdown-panel").slideDown(300);
+		$("#dropdown-panel").empty();
+		$("#dropdown-panel").append("<h1>HEM</h1>");
+		$("#dropdown-panel").css("background-color", "#D8514F");
+	});
+	
+	$("#omoss").mouseenter(function(){
+		$("#dropdown-panel").slideDown(300);
+		$("#dropdown-panel").empty();
+		$("#dropdown-panel").append("<h1>OM OSS</h1>");
+		$("#dropdown-panel").css("background-color", "#4F7FD8");
+	});
+	
+	$("#kontakt").mouseenter(function(){
+		$("#dropdown-panel").slideDown(300);
+		$("#dropdown-panel").empty();
+		$("#dropdown-panel").append("<h1>KONTAKT</h1>");
+		$("#dropdown-panel").css("background-color", "#4FD8AD");
+	});
+	
+	$("#bilder").mouseenter(function(){
+		$("#dropdown-panel").slideDown(300);
+		$("#dropdown-panel").empty();
+		$("#dropdown-panel").append("<h1>BILDER</h1><img src='http://animalia-life.com/data_images/cat/cat2.jpg'>");
+		$("#dropdown-panel").css("background-color", "#C14FD8");
+	});
+	
+	$("#dropdown-panel").mouseleave(function(){
+		$("#dropdown-panel").slideUp(300);
 	});
 });
